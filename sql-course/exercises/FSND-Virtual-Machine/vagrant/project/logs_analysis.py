4af5ba1225b15c81a4cb4ddadd56bdb6d64bafde
Waqas Ahmed <wakqasahmed@gmail.com>
2018-07-21 13:45:43 +0400

    Made it compliant with PEP8 requirements

--- sql-course/exercises/FSND-Virtual-Machine/vagrant/project/logs_analysis.py
+++ sql-course/exercises/FSND-Virtual-Machine/vagrant/project/logs_analysis.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
-# 
+#
 # A web service to display the analysis of news articles logs.
 
 from flask import Flask, request, redirect, url_for
 
-from logs_analysis_db import get_most_popular_articles, get_most_popular_article_authors, get_errorenous_requests_per_day_more_than_a_percent
+from logs_analysis_db import (get_most_popular_articles,
+                              get_most_popular_article_authors,
+                              get_errorenous_requests_per_day_gt_a_percent)
 
 app = Flask(__name__)
 
-# HTML template for the forum page
+# HTML template for the logs analysis page
 HTML_WRAP = '''\
 <!DOCTYPE html>
 <html>
   <head>
     <title>Logs Analysis</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400"
+    rel="stylesheet">
     <style>
       body { font-family: "Open Sans", Helvetica, sans-serif; }
       h1, form { text-align: center; }
@@ -31,7 +34,7 @@ HTML_WRAP = '''\
     <div>
     <h2>What are the most popular three articles of all time?</h2>
     <ul>%s</ul>
-    </div>   
+    </div>
 
     <div>
     <h2>Who are the most popular article authors of all time?</h2>
@@ -41,35 +44,45 @@ HTML_WRAP = '''\
     <div>
     <h2>On which days did more than 1%% of requests lead to errors?</h2>
     <ul>%s</ul>
-    </div> 
+    </div>
 
   </body>
 </html>
 '''
 
-# HTML template for an individual comment
+# HTML template for an individual article and its views
 MOST_POPULAR_ARTICLES = '''\
     <li>"%s" - %s views</li>
 '''
 
+# HTML template for an individual author and its views
 MOST_POPULAR_ARTICLE_AUTHORS = '''\
     <li>%s - %s views</li>
 '''
 
+# HTML template for errorenous request > 1 percent
 ERRORENOUS_REQUESTS = '''\
     <li>%s - %s%%</li>
 '''
 
+
 @app.route('/', methods=['GET'])
 def main():
-  '''Main page of the forum.'''
-  articles = "".join(MOST_POPULAR_ARTICLES % (title, views) for title, views in get_most_popular_articles())
-  authors = "".join(MOST_POPULAR_ARTICLE_AUTHORS % (author_name, views) for author_name, views in get_most_popular_article_authors())
-  errorenous_requests = "".join(ERRORENOUS_REQUESTS % (day.strftime("%b %d, %Y"), request_to_error_ratio) for day, request_to_error_ratio in get_errorenous_requests_per_day_more_than_a_percent())
+    '''Main page of the logs analysis.'''
+    articles = "".join(MOST_POPULAR_ARTICLES
+                       % (title, views) for title, views
+                       in get_most_popular_articles())
+    authors = "".join(MOST_POPULAR_ARTICLE_AUTHORS
+                      % (author_name, views) for author_name, views
+                      in get_most_popular_article_authors())
+    errorenous_requests = "".join(
+            ERRORENOUS_REQUESTS
+            % (day.strftime("%b %d, %Y"), request_to_error_ratio)
+            for day, request_to_error_ratio
+            in get_errorenous_requests_per_day_gt_a_percent())
 
-  html = HTML_WRAP % (articles, authors, errorenous_requests)
-  return html
+    html = HTML_WRAP % (articles, authors, errorenous_requests)
+    return html
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000)
-
+    app.run(host='0.0.0.0', port=8000)
--- sql-course/exercises/FSND-Virtual-Machine/vagrant/project/logs_analysis_db.py
+++ sql-course/exercises/FSND-Virtual-Machine/vagrant/project/logs_analysis_db.py
@@ -1,4 +1,4 @@
-# "Database code" for the DB Forum.
+# "Database code" for the DB News.
 
 import datetime
 import psycopg2
@@ -6,39 +6,35 @@ import bleach
 
 DBNAME = "news"
 
-'''Dummy data to test the HTML display'''
-MOST_POPULAR_ARTICLES = [("Princess Shellfish Marries Prince Handsome", "1201")]
-MOST_POPULAR_ARTICLE_AUTHORS = [("Ursula La Multa", "2304")]
-ERRORENEOUS_REQUESTS = [("July 29, 2016", "2.5")]
 
 def get_most_popular_articles():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect("dbname=news")
-  c = db.cursor()
-  c.execute("select * from most_popular_articles")
-  articles = c.fetchall()
-  db.close()
-  return articles
-  """return reversed(POSTS)"""
+    """Return three most accessed articles of all time,
+    sorted list with the most popular article at the top."""
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute("select * from most_popular_articles")
+    articles = c.fetchall()
+    db.close()
+    return articles
+
 
 def get_most_popular_article_authors():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect("dbname=news")
-  c = db.cursor()
-  c.execute("select * from most_popular_article_authors")
-  authors = c.fetchall()
-  db.close()
-  return authors
-  """return reversed(POSTS)"""
-
-def get_errorenous_requests_per_day_more_than_a_percent():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect("dbname=news")
-  c = db.cursor()
-  c.execute("select * from errorenous_requests_per_day_more_than_a_percent")
-  erroreneous_requests = c.fetchall()
-  db.close()
-  return erroreneous_requests
-  """return reversed(POSTS)"""  
+    """Return authors who got the most page views,
+    sorted list with the most popular author at the top."""
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute("select * from most_popular_article_authors")
+    authors = c.fetchall()
+    db.close()
+    return authors
 
 
+def get_errorenous_requests_per_day_gt_a_percent():
+    """Return day and error rate in percent for the days when
+    more than 1% of requests lead to errors."""
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute("select * from errorenous_requests_per_day_more_than_a_percent")
+    erroreneous_requests = c.fetchall()
+    db.close()
+    return erroreneous_requests
