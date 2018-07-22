f3b6a1879fcb7de5d44f0e589b00596864e9490e
Waqas Ahmed <wakqasahmed@gmail.com>
2018-07-23 00:37:33 +0400

    Refactored code and fixed a typo

--- sql-course/exercises/FSND-Virtual-Machine/vagrant/project/logs_analysis_db.py
+++ sql-course/exercises/FSND-Virtual-Machine/vagrant/project/logs_analysis_db.py
@@ -4,37 +4,35 @@ import datetime
 import psycopg2
 import bleach
 
-DBNAME = "news"
-
-
-def get_most_popular_articles():
+def get_most_popular_articles(conn):
     """Return three most accessed articles of all time,
     sorted list with the most popular article at the top."""
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
+    c = conn.cursor()
     c.execute("select * from most_popular_articles")
     articles = c.fetchall()
-    db.close()
     return articles
 
 
-def get_most_popular_article_authors():
+def get_most_popular_article_authors(conn):
     """Return authors who got the most page views,
     sorted list with the most popular author at the top."""
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
+    c = conn.cursor()
     c.execute("select * from most_popular_article_authors")
     authors = c.fetchall()
-    db.close()
     return authors
 
 
-def get_erroneous_requests_per_day_gt_a_percent():
+def get_erroneous_requests_per_day_gt_a_percent(conn):
     """Return day and error rate in percent for the days when
     more than 1% of requests lead to errors."""
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
+    c = conn.cursor()
     c.execute("select * from erroneous_requests_per_day_more_than_a_percent")
-    erroreneous_requests = c.fetchall()
-    db.close()
-    return erroreneous_requests
+    erroneous_requests = c.fetchall()
+    return erroneous_requests
+
+DBNAME = "news"
+conn = psycopg2.connect("dbname=" + DBNAME)
+most_popular_articles = get_most_popular_articles(conn)
+most_popular_article_authors = get_most_popular_article_authors(conn)
+erroneous_requests_per_day_gt_a_percent = get_erroneous_requests_per_day_gt_a_percent(conn)
+conn.close()
